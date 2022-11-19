e3c24f756a95ab35c48ce3a3ff466a07ecbad60c
Walid Bekkal <wbekkal@c2r4p7.42nice.fr>
2022-11-19 05:19:34 +0100

    const iterator, need fixes

--- vector.hpp
+++ vector.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <memory>
 #include "type_traits.hpp"
+#include "vector_iterators.hpp"
 /* 
 	- RANDOM ACCESS ITERATOR (ITERATOR THAT PROVIDES BOTH INCREMENT AND DECREMENT BUT ALSO METHODS
 	FOR MOVING FORWARD AND BACKWARD BY A SPECIFIED AMOUNT)
@@ -9,107 +10,6 @@
 */
 namespace ft
 {
-	// ====================  RANDOM ACCESS ITERATOR  ====================
-	template <class T>
-	class random_access_iterator
-	{
-		public:
-			typedef std::random_access_iterator_tag iterator_category;
-			typedef T value_type;
-			typedef std::ptrdiff_t difference_type;
-			typedef T *pointer;
-			typedef T &reference;
-
-			// ====================  CONSTRUCTORS  ====================
-			random_access_iterator() : _ptr(NULL){};
-			random_access_iterator(pointer ptr) : _ptr(ptr){};
-			random_access_iterator(const random_access_iterator &other) : _ptr(other._ptr){};
-
-			// ====================  DESTRUCTOR  ====================
-			~random_access_iterator(){};
-
-			// ====================  OPERATORS  ====================\
-				// ASSIGNATION
-			random_access_iterator &operator=(const random_access_iterator &other)
-			{
-				_ptr = other._ptr;
-				return *this;
-			};
-
-			// DEREFERENCING AS AN LVALUE
-			reference operator*() const { return *_ptr; };
-
-			// DEREFERENCING AS AN RVALUE
-			pointer operator->() const { return _ptr; };
-
-			// INCREMENTATION PREFIX
-			random_access_iterator &operator++()
-			{
-				_ptr++;
-				return *this;
-			};
-
-			// INCREMENTATION BY AN INTEGER
-			random_access_iterator operator++(int)
-			{
-				random_access_iterator tmp(*this);
-				operator++();
-				return tmp;
-			};
-
-			// DECREMENTATION PREFIX
-			random_access_iterator &operator--()
-			{
-				_ptr--;
-				return *this;
-			};
-
-			// DECREMENTATION BY AN INTEGER
-			random_access_iterator operator--(int)
-			{
-				random_access_iterator tmp(*this);
-				operator--();
-				return tmp;
-			};
-
-			// ADDITION BY AN INTEGER
-			random_access_iterator operator+(difference_type n) const { return random_access_iterator(_ptr + n); };
-
-			// SUBSTRACTION BY AN INTEGER
-			random_access_iterator operator-(difference_type n) const { return random_access_iterator(_ptr - n); };
-
-			// ADDITION ASSIGNATION BY AN INTEGER
-			random_access_iterator &operator+=(difference_type n)
-			{
-				_ptr += n;
-				return *this;
-			};
-
-			// SUBSTRACTION ASSIGNATION BY AN INTEGER
-			random_access_iterator &operator-=(difference_type n)
-			{
-				_ptr -= n;
-				return *this;
-			};
-
-			// SUBSTRACTION OF TWO ITERATORS
-			difference_type operator-(const random_access_iterator &other) const { return _ptr - other._ptr; };
-
-			// DEREFERENCING AN ITERATOR
-			reference operator[](difference_type n) const { return *(_ptr + n); };
-
-			// COMPARISON OPERATORS
-			bool operator==(const random_access_iterator &other) const { return _ptr == other._ptr; };
-			bool operator!=(const random_access_iterator &other) const { return _ptr != other._ptr; };
-			bool operator<(const random_access_iterator &other) const { return _ptr < other._ptr; };
-			bool operator<=(const random_access_iterator &other) const { return _ptr <= other._ptr; };
-			bool operator>(const random_access_iterator &other) const { return _ptr > other._ptr; };
-			bool operator>=(const random_access_iterator &other) const { return _ptr >= other._ptr; };
-
-		private:
-			pointer _ptr;
-	};
-
 	// ====================  VECTOR  ====================
 	template <class T, class Alloc = std::allocator<T> >
 	class vector
@@ -124,8 +24,8 @@ namespace ft
 			typedef const T *const_pointer;
 			typedef size_t size_type;
 			typedef ptrdiff_t difference_type;
-			typedef pointer iterator;
-			typedef const_pointer const_iterator;
+			typedef ft::random_access_iterator<T> iterator;
+			typedef ft::const_random_access_iterator<T> const_iterator;
 			// typedef std::reverse_iterator<iterator> reverse_iterator;
 			// typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
