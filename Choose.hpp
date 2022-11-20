8874eeb84dcc37a1dc5e894133fd1c5f38896d6c
Walid Bekkal <wbekkal@c1r1p5.42nice.fr>
2022-11-20 04:57:55 +0100

    choose function for const iterator/iterator

---
+++ Choose.hpp
@@ -0,0 +1,22 @@
+#ifndef CHOOSE_HPP
+#define CHOOSE_HPP
+
+namespace ft
+{
+	template <bool Flag, class IsTrue, class IsFalse>
+	struct choose;
+
+	template <class IsTrue, class IsFalse>
+	struct choose<true, IsTrue, IsFalse>
+	{
+		typedef IsTrue type;
+	};
+
+	template <class IsTrue, class IsFalse>
+	struct choose<false, IsTrue, IsFalse>
+	{
+		typedef IsFalse type;
+	};
+}
+
+#endif
--- VectorIterator.hpp
+++ VectorIterator.hpp
@@ -3,11 +3,16 @@
 
 #include "ChadIterator.hpp"
 #include "EnableIf.hpp"
+#include "Choose.hpp"
 
 namespace ft
 { 
-	
+	// isConst = true, type is const, otherwise type is not const
+	template <typename T, bool isConst>
+	class VectorIterator : public ft::ChadIterator<ft::random_access_iterator_tag, typename ft::choose<isConst, const T, T>::type>
+	{
 
+	}
 }
 
 #endif
